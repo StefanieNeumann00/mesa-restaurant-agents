cd97015f6f39ac3cd50bfc898ea2847b753baa8a
Johannes Ullrich <johannes.ullrich@dhbw-stuttgart.de>
2025-02-20 11:57:19 +0100

    Update Utils add food price

--- src/mesa_restaurant_agents/utils/order_status.py
+++ src/mesa_restaurant_agents/utils/order_status.py
@@ -1,4 +1,12 @@
 from enum import Enum
+from datetime import datetime, timedelta
+
+# Define food options with their prices
+food_options = {
+    "vegetarian": {"price": 20, "cost": 10},  # Cost is 50% of price
+    "meat": {"price": 30, "cost": 15},
+    "gluten_free": {"price": 25, "cost": 12.5}
+}
 
 # Enum to track the status of customer orders
 class OrderStatus(Enum):
@@ -6,6 +14,3 @@ class OrderStatus(Enum):
     ORDERED = 1    # Order has been placed
     PREPARING = 2  # Food is being prepared
     SERVED = 3     # Food has been delivered
-
-# Available food options in the restaurant
-food_options = ["vegetarian", "meat", "gluten free"]
