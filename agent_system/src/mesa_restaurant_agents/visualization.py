2db565fccaa4b6290aba44d3d35dd877cf166b70
Johannes Ullrich <johannes.ullrich@dhbw-stuttgart.de>
2025-02-25 00:19:36 +0100

    update visualization from steps to time of day

--- agent_system/src/mesa_restaurant_agents/visualization.py
+++ agent_system/src/mesa_restaurant_agents/visualization.py
@@ -7,69 +7,85 @@ import matplotlib.colors as mcolors
 import numpy as np
 from .utils.environment_definition import EnvironmentDefinition
 
+
 def display_mean_step_results(results):
     df = pd.DataFrame(results)
-    data_grouped = df.groupby(['Step']).agg(
-        mean_customer_count = ('Customer_Count', 'mean'),
-        mean_waiting_time = ('Average_Wait_Time', 'mean'),
-        mean_customer_satisfaction = ('Average_Customer_Satisfaction', 'mean'),
-        mean_profit = ('Profit', 'mean')).reset_index()
-    
+
+    # Convert steps to time of day
+    df['Time'] = df['Step'].apply(lambda x: pd.Timestamp('2024-01-01 11:00:00') + pd.Timedelta(minutes=x * 5))
+
+    data_grouped = df.groupby(['Time']).agg(
+        mean_customer_count=('Customer_Count', 'mean'),
+        mean_waiting_time=('Average_Wait_Time', 'mean'),
+        mean_customer_satisfaction=('Average_Customer_Satisfaction', 'mean'),
+        mean_profit=('Profit', 'mean')).reset_index()
+
     # Plotting the data in one plot
     fig, ax1 = plt.subplots(figsize=(15, 10))
 
-    ax1.plot(data_grouped['Step'], data_grouped['mean_customer_count'], marker='o', label='Customer Count')
-    ax1.plot(data_grouped['Step'], data_grouped['mean_waiting_time'], marker='o', label='Average Wait Time')
-    ax1.plot(data_grouped['Step'], data_grouped['mean_customer_satisfaction'], marker='o', label='Average Customer Satisfaction')
-    ax1.plot(data_grouped['Step'], data_grouped['mean_profit'], marker='o', label='Profit')
+    ax1.plot(data_grouped['Time'], data_grouped['mean_customer_count'], marker='o', label='Customer Count')
+    ax1.plot(data_grouped['Time'], data_grouped['mean_waiting_time'], marker='o', label='Average Wait Time')
+    ax1.plot(data_grouped['Time'], data_grouped['mean_customer_satisfaction'], marker='o',
+             label='Average Customer Satisfaction')
+    ax1.plot(data_grouped['Time'], data_grouped['mean_profit'], marker='o', label='Profit')
 
     ax1.set_title('Model Output Data over Steps')
-    ax1.set_xlabel('Step')
+    ax1.set_xlabel('Time of Day')
     ax1.set_ylabel('Values')
     ax1.legend()
 
-    print(plt.show())
+    plt.xticks(rotation=45)
+    plt.tight_layout()  # Adjust layout to prevent label cutoff
+    plt.show()
     return data_grouped
+
 
 def display_first_run_step_results_customer(results):
     df = pd.DataFrame(results)
-    data_first_run = df[df["RunId"]==0]
-    customer_infos_dict = dict(data_first_run["Customer_Info"])
-    customer_infos_list = [{**item, 'step': k} for k, v in customer_infos_dict.items() for item in v]
+    data_first_run = df[df["RunId"] == 0]
+
+    # Convert steps to time
+    data_first_run['Time'] = data_first_run['Step'].apply(
+        lambda x: pd.Timestamp('2024-01-01 11:00:00') + pd.Timedelta(minutes=x * 5))
+
+    customer_infos_dict = dict(zip(data_first_run["Time"], data_first_run["Customer_Info"]))
+    customer_infos_list = [{**item, 'time': k} for k, v in customer_infos_dict.items() for item in v]
     customer_infos_df = pd.DataFrame(customer_infos_list)
 
-    plots = ['waiting_time','order_status','satisfaction']
+    plots = ['waiting_time', 'order_status', 'satisfaction']
 
     for plot in plots:
-        fig = px.histogram(customer_infos_df, x="step", y=plot,
-                    color='customer_nr', barmode='group',
-                    height=400, nbins=len(customer_infos_df['step'].unique()))
+        fig = px.histogram(customer_infos_df, x="time", y=plot,
+                           color='customer_nr', barmode='group',
+                           height=400, nbins=len(customer_infos_df['time'].unique()))
+        fig.update_xaxes(title_text='Time of Day')
         print(fig.show())
     return customer_infos_df
 
 
 def display_first_run_step_results_waiter(results):
     df = pd.DataFrame(results)
-    data_first_run = df[df["RunId"]==0]
+    data_first_run = df[df["RunId"] == 0]
     waiter_infos_dict = dict(data_first_run["Waiter_Info"])
     waiter_infos_list = [{**item, 'step': k} for k, v in waiter_infos_dict.items() for item in v]
     waiter_infos_df = pd.DataFrame(waiter_infos_list)
 
-    plots = ['tips','avg_rating', 'served_customers']
+    plots = ['tips', 'avg_rating', 'served_customers']
 
     for plot in plots:
         fig = px.histogram(waiter_infos_df, x="step", y=plot,
-                    color='waiter_nr', barmode='group',
-                    height=400, nbins=len(waiter_infos_df['step'].unique()))
+                           color='waiter_nr', barmode='group',
+                           height=400, nbins=len(waiter_infos_df['step'].unique()))
         print(fig.show())
     return waiter_infos_df
+
 
 def visualize_grid(grid, ax):
     env, annot = grid.visualize()
     cmap = mcolors.ListedColormap(['#5C5A5A', '#CDCDCD', '#FFECA1', '#106366', '#FE9900', '#AA0F11'])
 
     # Create the heatmap
-    sns.heatmap(env, ax=ax, cmap=cmap, annot=annot, cbar=False, square=True, fmt="",)
+    sns.heatmap(env, ax=ax, cmap=cmap, annot=annot, cbar=False, square=True, fmt="", )
     #sns.heatmap(env, ax=ax, cmap="viridis", annot=annot, cbar=False, square=True, fmt="")
 
 
@@ -127,4 +143,3 @@ class GridAnimator:
             repeat=False
         )
         return ani
-    
